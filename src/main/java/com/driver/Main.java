cdea79b8c4b1bf421901b0c2be2a5c82c7b075cb
Pravin Bhagwat <pravinbhagwat150@gmail.com>
2022-12-10 19:35:50 +0530

    Encapsulation task completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        obj.name = "Pravin";
+//        'name' has private access in 'com.driver.RWOnly'
+        obj.setName("Pravin");
+        System.out.println(obj.getName());
+    }
 }
